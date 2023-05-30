40323667cf4085f76079eec0a1683d493b56276f
IsabellaSulaiman <135057336+IsabellaSulaiman@users.noreply.github.com>
2023-05-30 16:00:25 -0300

    change

--- EmpowerIS_1/EmpowerIS_1.cs
+++ EmpowerIS_1/EmpowerIS_1.cs
@@ -68,7 +68,8 @@ namespace EmpowerIS_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-	engine.GenerateInformation("Hello world")
-		}
+			engine.GenerateInformation("Hello world");
+			//change
+		} 
 	}
 }
