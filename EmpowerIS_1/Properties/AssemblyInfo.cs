f8e3a759f1ffd59a68ab70b3ee547e228a5e3c78
IsabellaSulaiman <135057336+IsabellaSulaiman@users.noreply.github.com>
2023-05-30 15:54:41 -0300

    Adicionar arquivos de projeto.

---
+++ EmpowerIS_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerIS_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerIS_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("B1FD09A4-22C9-424A-AF57-A41528424599")]
+[assembly: AssemblyVersion("1.0.0.0")]
